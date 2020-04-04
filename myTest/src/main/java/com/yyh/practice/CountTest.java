8e4422029f8058efe1c716f5edcfe0b2004c6188
yhyeoh <darkveloper@gmail.com>
2020-04-04 14:41:32 +0800

    MY: Test code

--- myTest/src/main/java/com/yyh/practice/CountTest.java
+++ myTest/src/main/java/com/yyh/practice/CountTest.java
@@ -2,11 +2,19 @@ package com.yyh.practice;
 
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class CountTest {
 	public static void main(String args[]) throws IOException {
-		
-		
+		System.out.println("test");
+
+        int buyAmt = 190000;
+        double rate = 2.3;
+        BigDecimal orgAmt = new BigDecimal(buyAmt);
+        BigDecimal dscAmt = (orgAmt.multiply(new BigDecimal(rate))).divide(new BigDecimal(100));
+
+        System.out.println("Result: " + dscAmt.setScale(4, RoundingMode.DOWN));
 	}
 	
 	
