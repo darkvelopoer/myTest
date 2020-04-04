96a9c703d571d8a2f687d2c6141ba4446e9a6f0e
yhyeoh <darkveloper@gmail.com>
2020-04-04 15:28:30 +0800

    MY: Test dir path

--- myTest/src/main/java/com/yyh/practice/CountTest.java
+++ myTest/src/main/java/com/yyh/practice/CountTest.java
@@ -2,12 +2,31 @@ package com.yyh.practice;
 
 
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class CountTest {
-	public static void main(String args[]) throws IOException {
-		
-		
-	}
-	
-	
+    public static void main(String args[]) throws IOException {
+        Path path = Paths.get("C:\\Users\\yhyeoh\\IdeaProjects\\untitled");
+
+        //no filter applyied
+        System.out.println("\nNo filter:");
+        try (DirectoryStream<Path> ds = Files.newDirectoryStream(path)) {
+            for (Path file : ds) {
+                System.out.println(file.getFileName());
+            }
+        }
+
+        System.out.println("\nFilter PNG, JPG and BMP files via glob pattern:");
+        try (DirectoryStream<Path> ds = Files.newDirectoryStream(path, "*.{png,jpg,bmp}")) {
+            for (Path file : ds) {
+                System.out.println(file.getFileName());
+            }
+        }
+
+    }
+
+
 }
