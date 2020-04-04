51a48c077cf570abd61a9c41c3bf7325526e0f9f
yhyeoh <darkveloper@gmail.com>
2020-04-04 15:28:30 +0800

    MY: Test dir path

--- myTest/src/main/java/com/yyh/practice/CountTest.java
+++ myTest/src/main/java/com/yyh/practice/CountTest.java
@@ -4,18 +4,41 @@ package com.yyh.practice;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class CountTest {
-	public static void main(String args[]) throws IOException {
+    public static void main(String args[]) throws IOException {
 		System.out.println("test");
 
-        int buyAmt = 190000;
-        double rate = 2.3;
-        BigDecimal orgAmt = new BigDecimal(buyAmt);
-        BigDecimal dscAmt = (orgAmt.multiply(new BigDecimal(rate))).divide(new BigDecimal(100));
+		int buyAmt = 190000;
+		double rate = 2.3;
+		BigDecimal orgAmt = new BigDecimal(buyAmt);
+		BigDecimal dscAmt = (orgAmt.multiply(new BigDecimal(rate))).divide(new BigDecimal(100));
 
-        System.out.println("Result: " + dscAmt.setScale(4, RoundingMode.DOWN));
-	}
-	
-	
+		System.out.println("Result: " + dscAmt.setScale(4, RoundingMode.DOWN));
+
+
+		Path path = Paths.get("C:\\Users\\yhyeoh\\IdeaProjects\\untitled");
+
+        //no filter applyied
+        System.out.println("\nNo filter:");
+        try (DirectoryStream<Path> ds = Files.newDirectoryStream(path)) {
+            for (Path file : ds) {
+                System.out.println(file.getFileName());
+            }
+        }
+
+        System.out.println("\nFilter PNG, JPG and BMP files via glob pattern:");
+        try (DirectoryStream<Path> ds = Files.newDirectoryStream(path, "*.{png,jpg,bmp}")) {
+            for (Path file : ds) {
+                System.out.println(file.getFileName());
+            }
+        }
+
+    }
+
+
 }
